933bd29d497221b533fd739c127214c1a60642d0
adri6uerrero <adrianseb3@gmail.com>
2024-12-12 20:05:59 +0100

    Agregar archivos de proyecto.

---
+++ Contactos/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Contactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAñadirContacto = new System.Windows.Forms.Button();
+            this.btnActualizarContacto = new System.Windows.Forms.Button();
+            this.btnEliminarContacto = new System.Windows.Forms.Button();
+            this.btnBuscarContacto = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lstContactos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(57, 68);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(57, 107);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero";
+            // 
+            // btnAñadirContacto
+            // 
+            this.btnAñadirContacto.Location = new System.Drawing.Point(57, 171);
+            this.btnAñadirContacto.Name = "btnAñadirContacto";
+            this.btnAñadirContacto.Size = new System.Drawing.Size(120, 23);
+            this.btnAñadirContacto.TabIndex = 4;
+            this.btnAñadirContacto.Text = "Añadir contacto";
+            this.btnAñadirContacto.UseVisualStyleBackColor = true;
+            this.btnAñadirContacto.Click += new System.EventHandler(this.btnAnadirContacto_Click);
+            // 
+            // btnActualizarContacto
+            // 
+            this.btnActualizarContacto.Location = new System.Drawing.Point(57, 200);
+            this.btnActualizarContacto.Name = "btnActualizarContacto";
+            this.btnActualizarContacto.Size = new System.Drawing.Size(120, 23);
+            this.btnActualizarContacto.TabIndex = 5;
+            this.btnActualizarContacto.Text = "Actualizar contacto";
+            this.btnActualizarContacto.UseVisualStyleBackColor = true;
+            this.btnActualizarContacto.Click += new System.EventHandler(this.btnActualizarContacto_Click);
+            // 
+            // btnEliminarContacto
+            // 
+            this.btnEliminarContacto.Location = new System.Drawing.Point(183, 171);
+            this.btnEliminarContacto.Name = "btnEliminarContacto";
+            this.btnEliminarContacto.Size = new System.Drawing.Size(107, 23);
+            this.btnEliminarContacto.TabIndex = 6;
+            this.btnEliminarContacto.Text = "Eliminar contacto";
+            this.btnEliminarContacto.UseVisualStyleBackColor = true;
+            this.btnEliminarContacto.Click += new System.EventHandler(this.btnEliminarContacto_Click);
+            // 
+            // btnBuscarContacto
+            // 
+            this.btnBuscarContacto.Location = new System.Drawing.Point(183, 200);
+            this.btnBuscarContacto.Name = "btnBuscarContacto";
+            this.btnBuscarContacto.Size = new System.Drawing.Size(107, 23);
+            this.btnBuscarContacto.TabIndex = 7;
+            this.btnBuscarContacto.Text = "Buscar contacto";
+            this.btnBuscarContacto.UseVisualStyleBackColor = true;
+            this.btnBuscarContacto.Click += new System.EventHandler(this.btnBuscarContacto_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(60, 258);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lstContactos
+            // 
+            this.lstContactos.FormattingEnabled = true;
+            this.lstContactos.Location = new System.Drawing.Point(318, 49);
+            this.lstContactos.Name = "lstContactos";
+            this.lstContactos.Size = new System.Drawing.Size(216, 212);
+            this.lstContactos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 302);
+            this.Controls.Add(this.lstContactos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBuscarContacto);
+            this.Controls.Add(this.btnEliminarContacto);
+            this.Controls.Add(this.btnActualizarContacto);
+            this.Controls.Add(this.btnAñadirContacto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAñadirContacto;
+        private System.Windows.Forms.Button btnActualizarContacto;
+        private System.Windows.Forms.Button btnEliminarContacto;
+        private System.Windows.Forms.Button btnBuscarContacto;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lstContactos;
+    }
+}
+
---
+++ Contactos/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contactos
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> contactos = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAnadirContacto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string numero = txtNumero.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(numero))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre y el número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (contactos.ContainsKey(nombre))
+            {
+                MessageBox.Show("El contacto ya existe. Usa el botón 'Actualizar' si deseas modificarlo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            contactos.Add(nombre, numero);
+            ActualizarListaContactos();
+            MessageBox.Show("Contacto añadido exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnActualizarContacto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string numero = txtNumero.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(numero))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre y el nuevo número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (contactos.ContainsKey(nombre))
+            {
+                contactos[nombre] = numero;
+                ActualizarListaContactos();
+                MessageBox.Show($"El número de {nombre} ha sido actualizado a {numero}.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El contacto no existe. Por favor, añádelo primero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminarContacto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre del contacto a eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (contactos.Remove(nombre))
+            {
+                ActualizarListaContactos();
+                MessageBox.Show($"El contacto {nombre} ha sido eliminado.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El contacto no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBuscarContacto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre del contacto a buscar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (contactos.TryGetValue(nombre, out string numero))
+            {
+                MessageBox.Show($"Número de {nombre}: {numero}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("El contacto no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ActualizarListaContactos()
+        {
+            lstContactos.Items.Clear();
+            foreach (var contacto in contactos)
+            {
+                lstContactos.Items.Add($"{contacto.Key}: {contacto.Value}");
+            }
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
